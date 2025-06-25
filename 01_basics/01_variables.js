c9b03352634555ec50664dd8ee8ef59e7fad4e84
tejasst2024 <166695969+tejasst2024@users.noreply.github.com>
2025-06-25 17:38:44 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accName = "Tejas"
+let accNumber = 12345
+var accEmail = "test@test.com"
+accIdentifier = 11111
+let accState
+
+// accName = "Random"
+
+accNumber = 23456
+accEmail = "newTest@test.com"
+accIdentifier = 22222
+
+/*
+* Prefer not to use var due to issues with block or function scope
+*/
+
+console.table([accName, accNumber, accEmail, accIdentifier, accState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat the js code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+// https://tc39.es/ecma262/
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique in react
+// object => {}
+
+console.log(typeof 18);        // string
+console.log(typeof null);      // object
+console.log(typeof undefined); // undefined
