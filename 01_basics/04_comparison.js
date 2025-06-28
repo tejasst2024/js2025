a91d55ff1c3d91ce2b61aabd5aecbbce71ee26d4
S <ts761@marriott.com>
2025-06-28 11:52:36 +0530

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+console.log(2 > 0); // true
+console.log(2 > 2); // false
+console.log(2 != 1);    // true
+
+// Converts to Number and does the comaprison
+console.log("2" > 0);   //  true
+console.log("02" > 1);  //  true
+
+// null to number is 0
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined > 0);  // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+
+// ===
+console.log("2" === 2); // false
+
+
+
+
