bf7cccfeca8d2b7227299ef3a706a3ce5bce37fa
S <ts761@marriott.com>
2025-06-28 16:34:00 +0530

    object 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,41 @@
+const tinderUser = new Object();
+tinderUser.id = "id1"
+tinderUser.name = "RandomName"
+tinderUser.isLoggedIn = true
+console.log(tinderUser);
+
+const regularUser = {
+    fullName: "RegularName",
+    email: "random@gmail.com",
+    dept: {
+        code: "IT01",
+        desc: "Information Technology"
+    }
+}
+console.log(regularUser?.dept?.code);
+
+const obj1 = {0: "a", 1: "b"}
+const obj2 = {2: "c", 3: "d"}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);  // { '0': 'a', '1': 'b', '2': 'c', '3': 'd' }
+
+const obj4 = {...obj1, ...obj2}
+console.log(obj4);  // { '0': 'a', '1': 'b', '2': 'c', '3': 'd' }
+
+const users = [
+    {
+        id: 1,
+        name: "Tejas"
+    },
+    {
+        id: 2,
+        name: "Rahul"
+    }
+]
+
+console.log(Object.keys(tinderUser));   // [ 'id', 'name', 'isLoggedIn' ]
+
+console.log(Object.entries(tinderUser));    //
+
+
+
