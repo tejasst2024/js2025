0cca83ee8ca4d9bc65b48c77c5e40c350c73d9b4
S <ts761@marriott.com>
2025-06-28 16:16:49 +0530

    object 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,54 @@
+// Singleton
+// Object.create
+const objSinglenton = new Object();
+
+// Not singleton
+// Literal
+const mySym = Symbol("1234")
+const obj = {
+    name: "Tejas",
+    age: 22,
+    "group Code": "IT",
+    [mySym] : "12345"
+}
+
+console.log(obj.name);  // Tejas
+// console.log(obj.group Code); // Does not work
+console.log(obj["group Code"]);
+console.log(obj[mySym]);
+
+obj.name = "Rahul"
+// Object.freeze(obj)
+obj.name = "Tejas"
+
+console.log(obj);
+
+obj.greeting = function() {
+    console.log(`Executed - ${this.age}`);
+    return "Greetings return"
+}
+
+console.log(obj);
+// {
+//   name: 'Tejas',
+//   age: 22,
+//   'group Code': 'IT',
+//   greeting: [Function (anonymous)],
+//   [Symbol(1234)]: '12345'
+// }
+
+console.log(obj.greeting);  // [Function (anonymous)]
+console.log(obj.greeting());
+// Executed -  22
+// undefined
+
+
+
+
+
+
+
+
+
+
+
